c4387ce94c71776cb018d3071797985d3ed1d19f
Guru200616 <168887288+Guru200616@users.noreply.github.com>
2025-11-01 21:05:37 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="brand">Glitt</div>
+      <nav className="site-nav">
+        <a href="#home">Home</a>
+        <a href="#icons">Icons</a>
+      </nav>
+    </header>
+  )
+}
